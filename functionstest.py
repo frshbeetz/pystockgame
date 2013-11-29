76e838e9ea180834670eb39bc9237ba5fc654256
Frshbeetz <kyomushugi@gmail.com>
2013-11-29 01:29:54 -0500

    Sell function works.

--- functionstest.py
+++ functionstest.py
@@ -1,8 +1,9 @@
-# This code is written poorly and doesn't work.
+# This code is written poorly and doesn't work well.
 current_balance = 1000
 stockprice = 100.0
 shares_held = 10
 shares_added = 0
+shares_removed = 0
 # If I knew what I was doing, these would probably be one function.
 def add_shares(shares_held,quantity):
 	return shares_held + quantity
@@ -14,8 +15,6 @@ def transaction_positive(current_balance,plus):
 	return current_balance + plus
 """	
 Fixed by welbornprod!
- For some reason calling the buy and sell functions breaks the program 
- on the grounds that current_balance is a string and not an integer... I can't figure out why.
 """
 def buy(stockprice, quantity, current_balance, balance_threshold=0):
 	cost = stockprice * quantity
@@ -31,9 +30,9 @@ def sell(stockprice, quantity, current_balance):
 	if shares_held  < quantity:
 		return (False, current_balance, 0)
 	else:
-		transaction_positive(current_balance,plus)
-		remove_shares(shares_held, quantity)
-		return (True, current_balance, shares_held)
+		current_balance = transaction_positive(current_balance,plus)
+		shares_removed = remove_shares(shares_held, quantity)
+		return (True, current_balance, shares_removed)
 
 # Asks the user explictly for B or S because I'm lazy.  Then calls the functions... or rather... breaks.
 
@@ -51,12 +50,24 @@ if buysell == "b":
 			print('Invalid amount given!')
 			quantity = None
 	
-	result, current_balance, shares_added= buy(stockprice, quantity, current_balance)
+	result, current_balance, shares_added = buy(stockprice, quantity, current_balance)
 	print "%r,%.2f,%d" % (result, current_balance, shares_added)
 
 elif buysell == "s":
-	quantity = raw_input("The stock is $" + str(stockprice) + " per share." " How many shares do you want to sell?: ")
-	print sell(stockprice, quantity, current_balance)
-
+	quantity = None
+	# loop until user inputs a valid number.
+	while not quantity:
+		print "You have " + str(shares_held) + " shares."
+		quantity = raw_input("The stock is $" + str(stockprice) + " per share." " How many shares do you want to sell?: ")
+		try:
+			# convert to int  number for math - no fraction shares.
+			quantity = int(quantity)
+		except:
+			# user entered non-number input
+			print('Invalid amount given!')
+			quantity = None
+	
+	result, current_balance, shares_removed = sell(stockprice, quantity, current_balance)
+	print "%r,%.2f,%d" % (result, current_balance, shares_removed)
 else:
 	print "Invalid Input"

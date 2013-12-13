3162446e9a39d0bbcc1bb56a29fdd0d4c707d2b7
Frshbeetz <kyomushugi@gmail.com>
2013-12-12 20:23:37 -0500

    added investment classes

--- source/stocks.py
+++ source/stocks.py
@@ -1,6 +1,6 @@
-#Should contain info about the state of a single player.
-#Info might include: Name, list of (ticker, shares_held) tuples,
-#account value, cash available, debts owed, etc.  
+#Should contain Stock info
+#Price, shares available
+#Description
 
 # Equities - These are the Companies
 # |	
@@ -113,9 +113,54 @@
 
 TICKERS
 
-Corporations [ULTR,MEGA,MULTI,UNIC,STCO,FACT,FARM,AIRC,LAND,GACO,COCO]
-Commoddities [OIL, COAL, GAS, STEEL, ALUM, WHT, COFF, LIVE]
-Precious Metals [AU, AG]
-Indexes [XWMI, XBLU, XIND, XTRA, XENC, XCOM, XNRG, XAGR, XAUG]
-Bonds [BOND]
+# Corporations [ULTR,MEGA,MULTI,UNIC,STCO,FACT,FARM,AIRC,LAND,GACO,COCO]
+# Commoddities [OIL, COAL, GAS, STEEL, ALUM, WHT, COFF, LIVE]
+# Precious Metals [AU, AG]
+# Indexes [XWMI, XBLU, XIND, XTRA, XENC, XCOM, XNRG, XAGR, XAUG]
+# Bonds [BOND]
 
+# These are the basic companies, each should respond to related commodities
+# in a more or less realistic way, IE: Oil prices being high will cause
+# lost revenue for transport companies.
+class Equities:
+
+	def __init__(self)
+		self.ticker = None
+		self.price = None
+		self.volatity = None
+		self.sharesAvail = None
+		self.desc = None
+
+# I know this is the same as above, but later on when we want to make a
+# distinction between the two, hopefully it'll make sense to do it like
+# this.
+class Commodities:
+
+	def __init__(self)
+		self.ticker = None
+		self.price = None
+		self.volatility = None
+		self.commodAvail = None
+		self.desc = None
+
+# Precious metals should move inversely to the markets. While not a rule IRL,
+# this will give players a means to "bet against" the whole market index.
+# Gold should start at a higher price than Silver.
+# Gold = high/mid volatility | Silver = low volatility
+class PrecMetals:
+	
+	def ___init__(self)
+		self.ticker = None
+		self.price = None
+		self.volatility = None
+
+# Indexes should be based on a calculation of the underlying stocks.
+# Whole Market Index should be an average of all Equities and Commod. prices.
+# Energy Co's index would be just an average of the Energy Companies prices.
+ 
+class Indexes:
+	
+	def __init__(self)
+		self.ticker = None
+		self.price = None
+		
